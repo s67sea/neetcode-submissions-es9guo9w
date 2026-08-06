359f2f8b3d7ac9b198aa887f0b1caf78d33f785e
Saahil Sundaresan <66180163+s67sea@users.noreply.github.com>
2026-08-06 21:15:21 +0200

    Add: sliding-window-maximum - submission-11

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-11.py
@@ -0,0 +1,22 @@
+import heapq
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        #we'll do the heap version which is O(n log n), since heap operations are O(log n)
+        q = []
+        res = []
+
+        for i in range(k):
+            heapq.heappush(q,(-nums[i],i))
+        res.append(-q[0][0])
+        for i in range(k,len(nums)):
+            heapq.heappush(q,(-nums[i],i))
+
+            while q[0][1] < (i-k+1):
+                heapq.heappop(q)
+            #while the "maximum" is out of range
+            #append the legit maximum to the output
+            res.append(-q[0][0])
+
+        
+        return res
