8b4633219b23b946abc0a332062714d9a05383ba
Saahil Sundaresan <66180163+s67sea@users.noreply.github.com>
2026-08-21 00:17:51 +0200

    Update: generate-parentheses - submission-1

--- Data Structures & Algorithms/generate-parentheses/submission-1.py
+++ Data Structures & Algorithms/generate-parentheses/submission-1.py
@@ -1,19 +1,27 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         res = []
-        stack = []
-        def compute(opened, closed):
-            if opened == closed == n:
-                res.append("".join(stack))
-                return
 
-            if opened < n:
-                stack.append("(")
-                compute(opened+1,closed)
-                stack.pop()
-            if closed < opened:
-                stack.append(")")
-                compute(opened,closed+1)
-                stack.pop()
-        compute(0,0)
+        combo = ""
+
+        def dfs(numOpen, numClosed):
+            nonlocal combo
+
+            if len(combo) == 2*n:
+                res.append(combo)
+                return
+            
+            #if numClosed < numOpen then you are allowed to close. you can never have more closed than open
+            if numClosed < numOpen:
+                combo += ")"
+                dfs(numOpen,numClosed+1)
+                combo = combo[:-1]
+
+            #if numOpen < budget(n) then you are allowed to open
+            if numOpen < n:
+                combo += "("
+                dfs(numOpen+1,numClosed)
+                combo = combo[:-1]
+        
+        dfs(0,0)
         return res
