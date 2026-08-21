ea13f7f5ce90a22e710561326befd9d7eba3c7a9
Saahil Sundaresan <66180163+s67sea@users.noreply.github.com>
2026-08-21 23:16:10 +0200

    Add: design-twitter-feed - submission-15

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-15.py
@@ -0,0 +1,42 @@
+from collections import defaultdict
+import heapq 
+
+class Twitter:
+
+    def __init__(self):
+        self.followlist = defaultdict(set) #map followee : follower
+        self.tweets = defaultdict(list) #for each user it's a list of tweetIds
+        self.count = 0 #counter for # of tweets to get a timestamp
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.tweets[userId].append((self.count,tweetId))
+        if len(self.tweets[userId]) > 10:
+            self.tweets[userId] = self.tweets[userId][-10:]
+        self.count += 1
+
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        q = []
+        #we need the values with the highest countId (i.e. recent ones), so we use a minheap
+        for followee in self.followlist[userId]:
+            #we need to process all of the tweets from this guy
+            #but this should be only up to 10 tweets
+            for count,tweetId in self.tweets[followee]:
+                heapq.heappush(q,(count,tweetId))
+        for count,tweetId in self.tweets[userId]:
+            heapq.heappush(q,(count,tweetId))
+        while len(q)>10:
+            heapq.heappop(q)
+        res = []
+        for i in range(len(q)):
+            count, tweetId = heapq.heappop(q)
+            res.append(tweetId)
+        return res[::-1]
+
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        self.followlist[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        self.followlist[followerId].discard(followeeId)
+        
